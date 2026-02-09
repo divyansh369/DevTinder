658188c466169f09702800a0aad3f350f041810d
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-02-10 01:07:15 +0530

    Playing with dynamic routes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 // use app.get() and app.post() to handle HTTP methods
-app.get("/user",(req,res) => {
+app.get("/user/:userId/:name/:password",(req,res) => {
+    console.log(req.params)
     res.send({"firstName":"John","lastName":"Doe"});
 })
 
