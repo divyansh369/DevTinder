8eaf41039d02c16cef11af4b584a26edf9225257
Divyansh Srivastava <divyanshsrivastava215@gmail.com>
2026-02-10 19:52:12 +0530

    Multiple router handler

--- src/app.js
+++ src/app.js
@@ -1,38 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-// use app.get() and app.post() to handle HTTP methods
-app.get("/user/:userId/:name/:password",(req,res) => {
-    console.log(req.params)
-    res.send({"firstName":"John","lastName":"Doe"});
-})
-
-app.post("/user",(req,res) => {
-    // saving user to database
-    res.send("User saved successfully 👍");
-});
-
-app.delete("/user",(req,res) => {
-    // deleting user from database
-    res.send("User deleted successfully !!!");
-});
-
-app.put("/user",(req,res) => {
-    // deleting user from database
-    res.send("User updated successfully !!!");
-});
-
 // use app.use() to handle all HTTP methods for a specific path
-app.use("/user", (req, res) => {
-  res.send("Hello from /user");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Response 1 !!!")
+    next();
+    res.send("Response 1 !!!");
+    console.log("Response 1 !!!");
+  },
+  (req, res,next) => {
+    console.log("Response 2 !!!");
+    next();
+    console.log("Response 2 !!!");
+  },
+  (req, res,next) => {
+    console.log("Response 3 !!!");
+    next();
+    console.log("Response 3 !!!");
+  },
+  (req, res,next) => {
+    console.log("Response 4 !!!");
+    // res.send("Response 4 !!!");
+    next();
+  },
+);
 
 // app.use("/", (req, res) => {
 //   res.send("Listening on /");
 // });
 
-
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
